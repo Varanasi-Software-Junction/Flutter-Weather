ce05336632626c1c8e4fee70b9d02c87256bc5ce
champaksworld <champaksworld@users.noreply.github.com>
2021-05-31 08:39:46 +0530

    Future without async and await

--- scratch.dart
+++ scratch.dart
@@ -2,23 +2,27 @@ import 'dart:io';
 import 'package:async/async.dart';
 void main()
 {
+callFunctions();
 
-  print("Start");
- f1();
- f2();
- f3();
-  print("Stop");
-
+}
+void callFunctions()
+{
+  f1();
+ String value= f2();
+ print("F2 $value");
+  f3();
 }
 void f1()
 {
   print("F1");
 }
-void f2()
+String f2()
 {
+  String result;
   Duration duration=Duration(seconds: 10);
   Future.delayed(duration,() {
     print("F2");
+    result:"F2";
   });
 }
 void f3()
